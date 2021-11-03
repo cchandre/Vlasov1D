1455be7a1f45a3d50d00dd053829848b3106e1ea
Cristel Chandre <cchandre@gmail.com>
2021-11-03 09:42:24 +0100

    Corrections of bugs in AnalyzeData
    - some bugs corrected in the matlab AnalyzeData (definition of BG modes)

--- VP1D4f_AnalyzeData.m
+++ VP1D4f_AnalyzeData.m
@@ -1,6 +1,6 @@
 function VP1D4f_AnalyzeData
 %%
-%% Last modified by Cristel Chandre (November 2, 2021)
+%% Last modified by Cristel Chandre (November 3, 2021)
 %% Comments? cristel.chandre@cnrs.fr 
 %%
 [filename, path] = uigetfile('*.mat');
@@ -20,24 +20,29 @@ if strcmp(output_modes,'real')==true
     colorbar
     xlabel('$\omega_p t$','interpreter','latex','FontSize',26)
     ylabel('$x$','interpreter','latex','FontSize',26)
-    set(gca,'box','on','FontSize',20,'LineWidth',2)    
+    set(gca,'box','on','FontSize',20,'LineWidth',2,'YDir','normal')    
     figure
     fft_E = fft2(suppl(:,2:end).');
     kx = pi/Lx*(-Nx/2:Nx/2-1);
-    om = (-Nt/2:Nt/2-1)/(Nt*d);
-    imagesc(om,kx,log(abs(fftshift(fft_E))))
+    om = 2*pi*(-Nt/2:Nt/2-1)/(Nt*d);
+    %imagesc(om,kx,log(abs(fftshift(fft_E))))
+    imagesc(om,kx,abs(fftshift(fft_E)))
     colorbar
-    xlim([-50 50]/(Nt*d))
-    ylim([-20 20]*pi/Lx)
-    caxis([-15 log(max(abs(fft_E(:))))])
+    xlim(2*pi*[-10 10]/(Nt*d))
+    ylim([-10 10]*pi/Lx)
+    %caxis([-15 log(max(abs(fft_E(:))))])
     xlabel('$\omega$','interpreter','latex','FontSize',26)
     ylabel('$k_x$','interpreter','latex','FontSize',26)
-    set(gca,'box','on','FontSize',20,'LineWidth',2) 
+    set(gca,'box','on','FontSize',20,'LineWidth',2,'YDir','normal') 
     hold on
-    lam02 = 1+5*double(kappa)/3*P0^(2/3)*kx.^2;
-    lambg2 = 1+3*P0/rho0*kx.^2;
-    lam = kx/sqrt(2).*sqrt(lam02-sqrt(lam02.^2-4*(lam02-lambg2)));
+    lam02 = 1+3*double(kappa)*P0^(2/3)/rho0*kx.^2;
+    lambg2 = 1+3*P0/rho0^2*kx.^2;
+    lam = 1/sqrt(2).*sqrt(lam02-sqrt(lam02.^2-4*(lam02-lambg2)));
     plot(lam, kx, 'o', 'MarkerEdgeColor','k','MarkerFaceColor','k','MarkerSize',10)
+    plot(-lam, kx, 'o', 'MarkerEdgeColor','k','MarkerFaceColor','k','MarkerSize',10)
+    lam = 1/sqrt(2).*sqrt(lam02+sqrt(lam02.^2-4*(lam02-lambg2)));
+    plot(lam, kx, 'o', 'MarkerEdgeColor','k','MarkerFaceColor','k','MarkerSize',10)
+    plot(-lam, kx, 'o', 'MarkerEdgeColor','k','MarkerFaceColor','k','MarkerSize',10)
 elseif isinteger(output_modes)==true
     for it=3:output_modes-1
         semilogy(suppl(:,1),abs(suppl(:,it)),'LineWidth',2,'DisplayName',strcat('$k=$ ',' ',num2str(it-2)))
