5bb86edd07ee758b8c0a64654d14dae7578ed422
Cristel Chandre <cchandre@gmail.com>
2021-11-04 08:13:34 +0100

    Update VP1D4f_dict.py

--- VP1D4f_dict.py
+++ VP1D4f_dict.py
@@ -6,7 +6,7 @@ import numpy as xp
 
 kappa = 7
 
-Tf = 50
+Tf = 20
 integrator_kinetic = 'position-Verlet'
 nsteps = 30
 integrator_fluid = 'RK45'
@@ -17,19 +17,19 @@ n_casimirs = 3
 
 Lx = 2 * xp.pi
 Lv = 4
-Nx = 2**9
-Nv = 2**9
+Nx = 2**10
+Nv = 2**10
 
 A = 1e-2
 k = 0.5
-f_init = lambda x, v: (1 - A * xp.cos(k * x)**3) * v**2 * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi)
+f_init = lambda x, v: (1 - A * xp.cos(k * x)) * v**2 * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi)
 
 output_var = 'E'
 output_modes = 'real'
 
 ## 'Compute', 'Plot' and/or 'Save'
 Kinetic = []
-Fluid = ['Compute', 'Plot', 'Save']
+Fluid = ['Compute', 'Plot']
 
 darkmode = True
 
