4e2c92ddfc8b93727c286e6d7d2ee5b75a82b48e
Cristel Chandre <cchandre@gmail.com>
2021-11-09 22:24:11 +0100

    Update VP1D4f_dict.py

--- VP1D4f_dict.py
+++ VP1D4f_dict.py
@@ -5,9 +5,15 @@
 import numpy as xp
 from scipy.special import erf
 
+<<<<<<< Updated upstream
 kappa = 1
 
 Tf = 200
+=======
+kappa = 1.001
+
+Tf = 100
+>>>>>>> Stashed changes
 integrator_kinetic = 'position-Verlet'
 nsteps = 16
 integrator_fluid = 'DOP853'
@@ -21,18 +27,29 @@ Lv = 4
 Nx = 2**11
 Nv = 2**11
 
+<<<<<<< Updated upstream
 A = 1e-4
 k = 0.5
 alpha = 0.1
 # f_init = lambda x, v: (1 - A * xp.cos(k * x)) * v**2 * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi)
 f_init = lambda x, v: (1 - A * xp.cos(k * x)) * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi) * (1 + erf(alpha * v / xp.sqrt(2)))
+=======
+A = 1e-3
+k = 0.5
+# f_init = lambda x, v: (1 - A * xp.cos(k * x)) * v**2 * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi)
+f_init = lambda x, v: (1 - A * xp.cos(k * x)) * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi)
+>>>>>>> Stashed changes
 
 output_var = 'E'
 output_modes = 'real'
 
 ## 'Compute', 'Plot' and/or 'Save'
 Kinetic = []
+<<<<<<< Updated upstream
 Fluid = ['Compute', 'Plot', 'Save']
+=======
+Fluid = ['Compute', 'Plot']
+>>>>>>> Stashed changes
 
 darkmode = True
 
