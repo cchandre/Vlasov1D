a0036ec3416d877618ec458fd5fb68bdb0a6f3f5
Cristel Chandre <cchandre@gmail.com>
2022-06-07 16:04:41 -0500

    Update VP1D4f_dict.py

--- VP1D4f_dict.py
+++ VP1D4f_dict.py
@@ -5,13 +5,13 @@
 import numpy as xp
 from scipy.special import erf
 
-kappa = 1.31
-Tf = 50
+kappa = 1.30834
+Tf = 80
 
 integrator_kinetic = 'position-Verlet'
-nsteps = 16
+nsteps = 40
 integrator_fluid = 'DOP853'
-precision = 1e-12
+precision = 1e-13
 
 n_casimirs = 3
 
@@ -20,14 +20,14 @@ Lv = 6
 Nx = 2**10
 Nv = 2**10
 
-A = 1e-7
+A = 1e-6
 k = xp.pi / Lx
 # alpha = 0.1
 f_init = lambda x, v: (1 - A * xp.cos(k * x)) * v**2 * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi)
 # f_init = lambda x, v: (1 - A * xp.cos(k * x)) * xp.exp(-v**2 / 2) / xp.sqrt(2 * xp.pi) * (1 + erf(alpha * v / xp.sqrt(2)))
 
 output_var = 'E'
-output_modes = 5
+output_modes = 8
 
 ## 'Compute', 'Plot' and/or 'Save'
 Kinetic = ['Compute', 'Plot', 'Save']
@@ -54,5 +54,5 @@ dict = {'kappa': kappa,
         'output_modes': output_modes,
         'Kinetic': Kinetic,
         'Fluid': Fluid,
-        'darkmode': darkmode})
+        'darkmode': darkmode}
 ###################################################################################################
