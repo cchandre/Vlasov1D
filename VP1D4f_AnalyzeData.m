c75b6a2c4c7fef793b0d265ffcdc6f4aa0b9d862
Cristel Chandre <cchandre@gmail.com>
2022-06-08 17:14:40 -0500

    Update VP1D4f_AnalyzeData.m

--- VP1D4f_AnalyzeData.m
+++ VP1D4f_AnalyzeData.m
@@ -57,3 +57,19 @@ elseif isinteger(output_modes)==true
     xlabel('$\omega_p t$','interpreter','latex','FontSize',26)
     set(gca,'box','on','FontSize',20,'LineWidth',2)    
 end
+
+%
+% Copyright (c) 2021 Cristel Chandre.
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms are permitted provided 
+% that the above copyright notice and this paragraph are duplicated in all 
+% such forms and that any documentation, advertising materials, and other 
+% materials related to such distribution and use acknowledge that the
+% software was developed by the CNRS. The name of the CNRS may not be used 
+% to endorse or promote products derived from this software without 
+% specific prior written permission.
+
+% THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED 
+% WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF 
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
