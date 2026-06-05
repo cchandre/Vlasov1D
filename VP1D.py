362e5d88a48e8cf35bcd018e1c7880dd78a0d675
Cristel Chandre <cchandre@gmail.com>
2026-06-05 16:42:27 -0400

    Update VP1D.py

--- VP1D.py
+++ VP1D.py
@@ -26,10 +26,21 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import numpy as np
-from pyfftw.interfaces.scipy_fft import rfft, irfft, rfftfreq
+from scipy.fft import rfft, irfft, rfftfreq
+# from pyfftw.interfaces.scipy_fft import rfft, irfft, rfftfreq
+# import pyfftw
 from scipy.integrate import trapezoid
+import matplotlib.pyplot as plt
 from dataclasses import dataclass
 from pyhamsys import HamSys
+
+# pyfftw.config.PLANNER_EFFORT = 'FFTW_ESTIMATE'
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='sans-serif', size=20)
+plt.rc('axes', labelsize=26)
+plt.rc('lines', linewidth=3)
+plt.rc('image', cmap='bwr')
 
 @dataclass
 class VP1Dparams:
@@ -39,6 +50,7 @@ class VP1Dparams:
     Nv: int
     precision: float
     f_init: callable
+    show_distribution: bool = False
 
 def _rfft(f, precision=None, tail_indx=None, axis=0):
     fft_f = rfft(f, axis=axis)
@@ -53,32 +65,34 @@ def _rfft(f, precision=None, tail_indx=None, axis=0):
 class VP1D(HamSys):
 	
     def __init__(self, params: VP1Dparams):
-        self.x = np.linspace(-params.Lx, params.Lx, params.Nx, endpoint=False, dtype=np.float64)
-        self.v = np.linspace(-params.Lv, params.Lv, params.Nv, endpoint=False, dtype=np.float64)
-        self.kx = np.pi / params.Lx * rfftfreq(params.Nx, d=1/params.Nx)
-        self.kv = np.pi / params.Lv * rfftfreq(params.Nv, d=1/params.Nv)
+        super().__init__()
+        self.Lx, self.Lv, self.Nx, self.Nv = params.Lx, params.Lv, params.Nx, params.Nv
+        self.x = np.linspace(-self.Lx, self.Lx, self.Nx, endpoint=False, dtype=np.float64)
+        self.v = np.linspace(-self.Lv, self.Lv, self.Nv, endpoint=False, dtype=np.float64)
+        self.kx = np.pi / self.Lx * rfftfreq(self.Nx, d=1/self.Nx)
+        self.kv = np.pi / self.Lv * rfftfreq(self.Nv, d=1/self.Nv)
         self.precision = params.precision
-        self.tail_indx_x = 3 * params.Nx // 8
-        self.tail_indx_v = 3 * params.Nv // 8
-        div = np.divide(1, 1j * self.kx, where=self.kx!=0)
-        div[0] = 0
-        self.f_init = params.f_init(self.x[:, None], self.v[None, :])
-        rho0 = trapezoid(self.f_init, self.v, axis=1)
-        self.E_init = irfft(div * _rfft(rho0, precision=self.precision, tail_indx=self.tail_indx_x, axis=0), axis=0)
+        self.tail_indx_x = 3 * self.Nx // 8
+        self.tail_indx_v = 3 * self.Nv // 8
+        self.div = np.divide(1, 1j * self.kx, where=self.kx!=0, out=None)
+        self.div[0] = 0
+        self.f0 = params.f_init(self.x[:, None], self.v[None, :])
+        if params.show_distribution:
+            self.ax, self.im = self.plot_f(self.f0)
+        
+    def compute_E(self, f):
+        rho = trapezoid(f, self.v, axis=1)
+        return irfft(self.div * _rfft(rho, precision=self.precision, tail_indx=self.tail_indx_x, axis=0), axis=0)
 
-    def chi(self, h, t, y):
-        f, E = y
+    def chi(self, h, t, f):
         f_ = irfft(np.exp(-1j * self.kx[:, None] * self.v[None, :] * h) * _rfft(f, precision=self.precision, tail_indx=self.tail_indx_x, axis=0), axis=0)
-        E_ = self.E(trapezoid(f_, self.v, axis=1))
-        f = irfft(np.exp(-1j * E_[:, None] * self.kv[None, :] * h) * _rfft(f_, precision=self.precision, tail_indx=self.tail_indx_v,axis=1), axis=1)
-        return f, E_
+        E = self.compute_E(f_)
+        return irfft(np.exp(-1j * E[:, None] * self.kv[None, :] * h) * _rfft(f_, precision=self.precision, tail_indx=self.tail_indx_v,axis=1), axis=1)
 
-    def chi_star(self, h, t, y):
-        f, E = y
+    def chi_star(self, h, t, f):
+        E = self.compute_E(f)
         f_ = irfft(np.exp(-1j * E[:, None] * self.kv[None, :] * h) * _rfft(f, precision=self.precision, tail_indx=self.tail_indx_v, axis=1), axis=1)
-        f_ = irfft(np.exp(-1j * self.kx[:, None] * self.v[None, :] * h) * _rfft(f_, precision=self.precision, tail_indx=self.tail_indx_x, axis=0), axis=0)
-        E_ = self.E(trapezoid(f_, self.v, axis=1))
-        return f_, E_
+        return irfft(np.exp(-1j * self.kx[:, None] * self.v[None, :] * h) * _rfft(f_, precision=self.precision, tail_indx=self.tail_indx_x, axis=0), axis=0)
     
     def compute_moments(self, f, n, centered=True):
         v = self.v
@@ -97,8 +111,30 @@ class VP1D(HamSys):
                 table_moments.append(Sm)
         return np.array(table_moments)
     
-    def energy_kinetic(self, f, E):
-        return (trapezoid(trapezoid(self.v[None, :]**2 * f, self.v, axis=1), self.x) + trapezoid(E**2, self.x)) / 2
+    def hamiltonian(self, t, f):
+        f_ = f if f.shape == (self.Nx, self.Nv) else f.T
+        E = self.compute_E(f_)
+        kinetic = trapezoid(trapezoid(self.v[None, :]**2 * f_, self.v, axis=1), self.x)
+        potential = trapezoid(E**2, self.x)
+        return (kinetic + potential) / 2
     
     def casimirs_kinetic(self, f, n):
         return [trapezoid(trapezoid(f**m, self.v, axis=1), self.x) for m in range(1, n+1)]
+    
+    def visualize_f(self, t, f, t_eval):
+        if hasattr(self, 'ax') and np.any(np.isclose(t, t_eval, atol=1e-5)):
+            self.im.set_data(f.transpose())
+            self.ax.set_title(f"$t = {t:.1f}$", loc='right', pad=-10)
+            self.ax.figure.canvas.draw_idle()
+            plt.pause(0.01)
+    
+    def plot_f(self, f):
+        fig, ax = plt.subplots(figsize=(10, 6.5))
+        fig.canvas.manager.set_window_title(r'Distribution function f(x,v,t)')
+        ax.set_title('$t = 0.0$', loc='right', pad=-10)
+        im = ax.imshow(f.transpose(), interpolation='gaussian', origin='lower', aspect='auto', extent=(-self.Lx, self.Lx, -self.Lv, self.Lv))
+        ax.set_xlabel('$x$')
+        ax.set_ylabel('$v$')
+        fig.colorbar(im, ax=ax)
+        plt.pause(0.01)
+        return ax, im
